54eeb8903e3ebe98288831fb59ae74598ea7c175
agentemy <artemy.kapitsyn@yandex.ru>
2026-05-18 19:40:45 +0300

    Config GitHub pages deploy v.1.2

--- src/config.js
+++ src/config.js
@@ -3,9 +3,7 @@
 export const CONFIG = {
   APP_NAME: 'HR Assistant',
   MODE: 'static',
-
-  DATA_URL: './src/data/recruiting.json',
-
+  DATA_URL: './data/recruiting.json',
   API_ENABLED: false,
   API_BASE: ''
 };
